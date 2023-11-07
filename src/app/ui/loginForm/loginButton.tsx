0b064e9c7bbbcccb168633342a7add2b15319400
Davide Filippi <9254428-Vadied@users.noreply.gitlab.com>
2023-11-07 19:01:52 +0100

    add login

---
+++ src/app/ui/loginForm/loginButton.tsx
@@ -0,0 +1,15 @@
+import { useFormStatus } from "react-dom";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import style from "./style.module.css";
+
+import Button from "@/app/ui/button";
+
+export default function LoginButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button aria-disabled={pending}>
+      Log in <ArrowRightIcon />
+    </Button>
+  );
+}
